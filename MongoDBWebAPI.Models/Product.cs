f8cd987715f29d9ef3386fbec80a719ba1acfe45
ABU Rayhan <rayhan@ibos.io>
2023-09-07 12:38:08 +0600

    Fluent Validation

---
+++ MongoDBWebAPI.Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace MongoDBWebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(product => product.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must be less than 100 characters.");
+
+            RuleFor(product => product.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
+}
---
+++ MongoDBWebAPI/Controllers/FluentValidationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDBWebAPI.Models;
+
+namespace MongoDBWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FluentValidationController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            // Validate the product
+            var validator = new ProductValidator();
+            var validationResult = validator.Validate(product);
+
+            if (!validationResult.IsValid)
+            {
+                // If validation fails, return the error messages to the user
+                foreach (var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+
+                return BadRequest(product);
+            }
+
+            // If validation passes, perform the action (e.g., save to the database)
+            // ...
+
+            return Ok();
+        }
+    }
+}
